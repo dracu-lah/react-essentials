82613278bbd9358fc54a9f427d01251d1badaf37
dracu <dracu@thonkpad.thonkpad>
2025-04-22 22:18:17 +0530

    feat added chapter one

--- react-quickstart/basics/src/App.jsx
+++ react-quickstart/basics/src/App.jsx
@@ -1,6 +1,15 @@
+import Chapter1 from "./components/Chapter1"
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1>
+        React App
+      </h1>
+      <div>
+        <Chapter1 />
+      </div>
+    </div>
   )
 }
 
---
+++ react-quickstart/basics/src/components/Chapter1/index.jsx
@@ -0,0 +1,14 @@
+const Chapter1 = () => {
+  const employee = {
+    name: "Nevil",
+    age: 23
+  }
+  return (
+    <div>
+      <h1>Name: {employee.name}</h1>
+      <h2>Age: {employee.age}</h2>
+    </div>
+  )
+}
+
+export default Chapter1
